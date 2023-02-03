a4004287e202010afeff14e9049f94f6ec8c906c
beckykowalski <becky.kowalski1129@gmail.com>
2023-02-03 14:08:25 -0500

    added velocity flux constants for AM

--- DMConstants.py
+++ DMConstants.py
@@ -56,9 +56,10 @@ sectoyear = 1./ (60.*60.*24.*365)
 ##### define constants
 
 vE = 230. # km / s, Earth's nominal velocity
-vEsc = 600. # km / s, Galactic Escape veloicty
-#v0 = 220. # km / s, Maxwellian DM velocity distribution
+vEsc = 544. # km / s, Galactic Escape veloicty
 v0 = 238. # km / s, Maxwellian DM velocity distribution
+vEavg = 29.8 # km / s , Earth's average galactocentric speed 
+
 #### define mathematical converstion factors
 
 # 1 GeV/c^2 = 1.7827 * 10^-27 kg
@@ -93,3 +94,25 @@ k0 = (np.pi * v0*v0)**(3./2.)
 
 
 rho = 0.3 # GeV / (c^2 cm^3), DM local density
+
+# Earth's orbit vectors, taken from https://arxiv.org/pdf/2105.00599.pdf
+
+vEarth_R_sin = -0.0505
+vEarth_Theta_sin = 0.4944
+vEarth_Phi_sin = -0.8677
+vEarth_R_cos = 0.9941
+vEarth_Theta_cos = 0.1088
+vEarth_Phi_cos = 0.0042
+timeometa = 0.0172 # units: days^-1
+deltaT = 0.218 # unit years: should be March 22, 2018. Can make unit of days
+
+# Solar peculiar velocities, taken from https://arxiv.org/pdf/2105.00599.pdf
+## units in km/s
+
+vSolar_R = 11.1
+vSolar_Theta = 12.2
+vSolar_Phi = 7.3
+
+
+
+

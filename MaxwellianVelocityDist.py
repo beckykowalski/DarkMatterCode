16f3d12e6fe3a652d63529a1a4f237dda77b716d
beckykowalski <becky.kowalski1129@gmail.com>
2023-02-22 16:49:57 -0500

    added sm/s0 calc

--- MaxwellianVelocityDist.py
+++ MaxwellianVelocityDist.py
@@ -7,16 +7,22 @@ import numpy as np
 ### sigma = velocity dispersion
 ### analytical form of vmin to vescape velocity integral f(v, sigma) / v
 
-def MaxwellianVelocityDistribution(k0, k1, R0, r, E0, v0, vE, vEsc, vmin):
+def MaxwellianVelocityDistribution(k0, k1, R0, r, E0, v0, vE, vEsc, vmin, mod, vmod = 0.):
     rateval = 0
     
     kFactor = k1 / k0
     RateFactor = R0 / (E0 * r)
     
     x = vmin/v0
-    y = vE/v0 # when time dependence is introduced, this becomes : (vsun+vearth(t))/v0
     z = vEsc/v0
+
+    y = 0.
     
+    if mod == False:
+        y = vE/v0 # when time dependence is introduced, this becomes : (vsun+vearth(t))/v0
+    if mod == True:
+        y = vmod
+        
     exponential = np.exp( -(z*z) )
     
     normalization = 1. / (sp.special.erf(z) - 2./np.sqrt(np.pi)*z*exponential)
@@ -58,3 +64,13 @@ def VelocitySum(v0, vE):
     phi = phi/(2.*np.pi) + earthT0 # units in years
 
     return MaxModVel, MinModVel, AvgModVel, phi
+
+def AnnualModulationEventRate(k0, k1, R0, r, E0, v0, vE, vEsc, vmin, vEAvg):
+    
+    MaxYV, MinYV, MeanYV, phi = VelocitySum(v0, vEAvg)
+    
+    
+    S0 = MaxwellianVelocityDistribution(k0, k1, R0, r, E0, v0, vE, vEsc, vmin, True, MeanYV)
+    Sm = (MaxwellianVelocityDistribution(k0, k1, R0, r, E0, v0, vE, vEsc, vmin, True, MaxYV) - MaxwellianVelocityDistribution(k0, k1, R0, r, E0, v0, vE, vEsc, vmin, True, MinYV) )/ 2.
+
+    return S0, Sm, phi

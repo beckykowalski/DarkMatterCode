376e38ae30c556b68761c48b5f2564dd4a8a57d2
beckykowalski <becky.kowalski1129@gmail.com>
2023-02-22 15:51:34 -0500

    remove duplicates

--- DMConstants.py
+++ DMConstants.py
@@ -24,11 +24,6 @@ m2tocm2 = 10000.
 sectoday = 1./ (60.*60.*24.)
 sectoyear = 1./ (60.*60.*24.*365)
 
-##### define constants
-
-vE = 230. # km / s, Earth's nominal velocity
-vEsc = 600. # km / s, Galactic Escape veloicty
-v0 = 238. # km / s, Maxwellian DM velocity distribution
 ##### define mathematical converstion factors
 
 # 1 GeV/c^2 = 1.7827 * 10^-27 kg
@@ -87,9 +82,7 @@ sectoyear = 1./ (60.*60.*24.*365)
 ##### define constants
 
 # for Maxwellian Velocity Distribution 
-vE = 230. # km / s, Earth's nominal velocity
-vEsc = 600. # km / s, Galactic Escape veloicty
-v0 = 238. # km / s, Maxwellian DM velocity distribution
+
 k0 = (np.pi * v0*v0)**(3./2.)
 
 

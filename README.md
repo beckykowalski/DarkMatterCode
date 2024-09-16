eac97ef9c8f9542549b061ea2b0665db3303384e
beckykowalski <becky.kowalski1129@gmail.com>
2024-09-16 13:47:44 -0400

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # DarkMatterCode
+
+To calculate event rate:
+   RunScripts.py
+   Input for CalculateEventRate:
+   	 <cross section>, <M_DM>, <List of elements>, <starting energy>, <ending energy>, <bins in calculation>, <resolution>
+   Function returns for the list of energies provided:
+   	 <list of energies>, <list of Sm>, <list of S0>, <list of Form Factor>, <list of x values>
+	 currently <list of x values> is empty, should be filled with v_min/v0 eventually for debugging purposes
+
+
+   An intermediate function exists: RunEventRateCalculation.py, to make a plot and text file of calculated event rate
+
+
+To calculate AM sensitivity:
+   RunSensitivity.py
+   Input for SensitivityCalculation:
+   	 <cross section>, <M_DM>, <exposure>, <CL value>, <list of elements>, <bkgRate>, <starting energy>, <ending energy>, <bins in calculation>, <resolution>
+   Change name of txt (line 33) to save mass, cross section in a text file
+
--- RunScripts.py
+++ RunScripts.py
@@ -11,16 +11,24 @@ import VectorAddition
 from VectorAddition import *
 import RunEventRateCalculation
 from RunEventRateCalculation import *
+import CalculateSensitivity
+from CalculateSensitivity import *
+
 
 sig = 10.**(-45.)
 mchi = 100.
-#MElements = [[AtomicMassInkg(132), 132, 1.]]
+MElements = [[AtomicMassInkg(132), 132, 1.]]
 #MElements = [[AtomicMassInkg(130), 130, .2725*.8], [AtomicMassInkg(128), 128, .2538*.8], [AtomicMassInkg(126), 126, .1506*.8], [AtomicMassInkg(125), 125, .0565*.8], [AtomicMassInkg(124), 124, .0379*.8], [AtomicMassInkg(123), 123, .0071*.8], [AtomicMassInkg(122), 122, .0204*.8], [AtomicMassInkg(121), 121, .0008*.8], [AtomicMassInkg(16), 16, .2]]
+#MElements = [[AtomicMassInkg(130), 130, 1.]]
+#MElements = [[AtomicMassInkg(130), 130, .2725], [AtomicMassInkg(128), 128, .2538], [AtomicMassInkg(126), 126, .1506], [AtomicMassInkg(125), 125, .0565], [AtomicMassInkg(124), 124, .0379], [AtomicMassInkg(123), 123, .0071], [AtomicMassInkg(122), 122, .0204], [AtomicMassInkg(121), 121, .0008]]
 #MElements = [[AtomicMassInkg(127.60), 127.6, 1.]]
 #MElements = [[AtomicMassInkg(127.60), 127.6, .8], [AtomicMassInkg(16), 16, .2]]
-#MElements = [[AtomicMassInkg(16), 16, .2],[AtomicMassInkg(127.60), 127.6, .8]]
-MElements = [[AtomicMassInkg(16), 16, .2], [AtomicMassInkg(127.60), 127.6, .8]]
-#MElements = [[AtomicMassInkg(6.941), 6.941, .079855], [AtomicMassInkg(16), 16, .368142], [AtomicMassInkg(100), 100, 55.2003]]
+#MElements = [[AtomicMassInkg(6), 6, .0758*.079855], [AtomicMassInkg(7), 7, 0.9241*0.079855], [AtomicMassInkg(16), 16, .368142], [AtomicMassInkg(100), 100, .552003]]
+#MElements = [ [AtomicMassInkg(6.941), 6.941, 0.079855], [AtomicMassInkg(16), 16, .368142], [AtomicMassInkg(100), 100, .552003]]
 
-#CalculateEventRate(sig, mchi, MElements, 0, 100, 1000, 20)
-CalculateEventRate(sig, mchi, MElements, 0, 100, 1000, 0)
+#CalculateEventRatePlotter(sig, mchi, MElements, 1.4, 9.2, 1000, 200)
+CalculateEventRatePlotter(sig, mchi, MElements, 0, 300, 3000, 0)
+#CalculateEventRatePlotter(sig, mchi, MElements, 10, 15, 1, 20)
+#CalculateEventRate(sig, mchi, MElements, 0, 100, 1000, 0)
+
+# calculating sensitivity for 10-15 keV energy bin, just one bin, 0 keV resolution SI 
